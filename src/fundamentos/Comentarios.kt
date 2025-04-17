7bc12b1c853e2ef69fd6d487a66665321972b04c
Pablo Alves Neri <pabloneri66@gmail.com>
2025-04-17 15:28:48 -0300

    Comentários e variaveis

---
+++ src/fundamentos/Comentarios.kt
@@ -0,0 +1,17 @@
+package fundamentos
+
+/**
+ * O kotlin Suporta Kdoc
+ * @param args Lista de parametros passados por linha de comando.
+ O Cravo brigou com a rosa.
+ */
+fun main(args: Array<String>) {
+    // comentário de uma linha
+    println("OPA")
+
+    /* Comentário
+    de Múltiplas linhas
+     */
+    print("Sério?")
+    
+}
