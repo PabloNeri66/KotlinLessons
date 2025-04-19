83c0b26703d46fb5fed7c35584d8ac75d2604685
Pablo Alves Neri <pabloneri66@gmail.com>
2025-04-18 22:00:05 -0300

    fundamentos

--- src/fundamentos/TiposBásicos.kt
+++ src/fundamentos/TiposBásicos.kt
@@ -10,6 +10,7 @@ fun main(args: Array<String>) {
     val num5: Float = 3.14F
     val num6: Double = 3.14
 
+
     // Tipo Caractere
     val char: Char = '?' // Outros exemplos... '1', 'g', ' '
 
@@ -18,10 +19,10 @@ fun main(args: Array<String>) {
 
     println(listOf(num1, num2, num3, num4, num5, num6, char, boolean))
 
+
     println(2 is Int)
     println(2147483648 is Long)
     println(1.0 is Double)
-
-    // Tudo é objeto
+     //Tudo é objeto
     println(10.dec())
 }
