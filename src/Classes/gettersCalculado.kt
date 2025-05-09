1db740a79d4eb8ec5ffcb7f24ea2f74b26781aa5
Pablo Alves Neri <pabloneri66@gmail.com>
2025-05-09 20:43:57 -0300

    Classes

--- src/Classes/gettersCalculado.kt
+++ src/Classes/gettersCalculado.kt
@@ -1,3 +1,22 @@
 package Classes
 
-class
+data class Produto(val nome:String, var preco:Double, val desc:Double, val ativo:Boolean){
+    val inativo: Boolean get() = !ativo
+    val precoComDesconto: Double get() = preco * (1 - desc)
+}
+
+fun main() {
+    val p1 = Produto("ipad", 2300.00, 0.20, ativo = true)
+    println(p1.precoComDesconto)
+
+    val p2 = Produto("galaxy s20", 2000.00, 0.5, false)
+    println("${p2.nome} De R$ ${p2.preco} por R$ ${p2.precoComDesconto}")
+
+    if (p2.inativo) {
+        p2.preco = 0.0
+        println("${p2.precoComDesconto}")
+    }
+
+    val (nome, preco) = Produto(nome = "Arroz", preco = 2500.00, 0.3, true)
+    println ("$nome, $preco")
+}
