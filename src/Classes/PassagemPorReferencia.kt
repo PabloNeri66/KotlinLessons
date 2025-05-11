d6078df990b5008828b99f2f8fd982173bc923bd
Pablo Alves Neri <pabloneri66@gmail.com>
2025-05-10 22:35:28 -0300

    Classes

---
+++ src/Classes/PassagemPorReferencia.kt
@@ -0,0 +1,29 @@
+package Classes
+
+
+// Error!! Kotlin: Val Cannot be reassigned
+//fun porRef(velocidade: Int){
+//    velocidade++
+//}
+
+data class Carro(var marca: String, var modelo: String, var velocidade: Int = 0)
+
+fun porReferencia(carro: Carro){
+    carro.velocidade++
+}
+
+
+fun main() {
+    var carro1 = Carro("Ford", "Fusion")
+
+    var carro2 = carro1
+    carro2.modelo = "Edge"
+    println(carro1)
+
+    carro1 = Carro("Audi", "A4")
+
+    porReferencia(carro1)
+    porReferencia(carro2)
+
+    println("$carro1 e $carro2")
+}
