5522dd5e04e243012461c38bf03330b1e7f1d5ba
Pablo Alves Neri <pabloneri66@gmail.com>
2025-05-14 17:05:50 -0300

    classes

--- src/Lambdas/SortedBy.kt
+++ src/Lambdas/SortedBy.kt
@@ -3,5 +3,7 @@ package Lambdas
 fun main() {
     val nomes = arrayListOf("Renata", "José", "Anderson", "Zélia")
     val ordenados = nomes.sortedBy { it }
+    val ordenadoUltimaLetra = nomes.sortedBy { it.reversed() }
     println(ordenados)
+    println(ordenadoUltimaLetra)
 }
